7579f97858d4a6c13bb3b5c0003d12871a30cd46
Eric Mann <eric@eamann.com>
2015-03-12 20:07:51 -0700

    Fix CLI requests

--- lib/vagrant-ghost/command.rb
+++ lib/vagrant-ghost/command.rb
@@ -8,18 +8,8 @@ module VagrantPlugins
 			end
 
 			def execute
-				options = {}
-				opts = OptionParser.new do |o|
-					o.banner = 'Usage: vagrant ghost [vm-name]'
-					o.separator ''
-
-					o.on('--provider provider', String, 'Update machines with the specific provider.') do |provider|
-						options[:provider] = provider
-					end
-				end
-
-				argv = parse_options(opts)
-				options[:provider] ||= @env.default_provider
+				cmd, cmd_args, argv, options = parse_args
+				cmd && cmd_args or return nil
 
 				@ui = @env.ui
 
@@ -50,6 +40,40 @@ module VagrantPlugins
 				end
 
 				false
+			end
+
+			private
+
+			def parse_args
+				options = {}
+				opts = OptionParser.new do |o|
+					o.banner = 'Usage: vagrant ghost [vm-name]'
+					o.separator ''
+
+					o.on('-h', '--help', 'Print this help') do
+						safe_puts(opts.help)
+					end
+
+					o.on('--provider provider', String, 'Update machines with the specific provider.') do |provider|
+						options[:provider] = provider
+					end
+				end
+
+				argv = split_main_and_subcommand(@argv.dup)
+				exec_args, cmd, cmd_args = argv[0], argv[1], argv[2]
+
+				# show help
+				if !cmd || exec_args.any? { |a| a == '-h' || a == '--help' }
+					safe_puts(opts.help)
+					return nil
+				end
+
+				options[:provider] ||= @env.default_provider
+
+				# remove extra "--" arge added by Vagrant
+				cmd_args.delete_if { |a| a == '--' }
+
+				return cmd, cmd_args, parse_options(opts), options
 			end
 		end
 	end
