76619a7b1b17993f3c8b84451db52452c76d371a
Eric Mann <eric@eamann.com>
2015-04-09 14:21:31 -0700

    Whitelist the hosts accessor

--- lib/vagrant-ghost/config.rb
+++ lib/vagrant-ghost/config.rb
@@ -3,7 +3,7 @@ require "vagrant"
 module VagrantPlugins
 	module Ghost
 		class Config < Vagrant.plugin("2", :config)
-			attr_accessor :aliases
+			attr_accessor :hosts
 			attr_accessor :id
 		end
 	end
